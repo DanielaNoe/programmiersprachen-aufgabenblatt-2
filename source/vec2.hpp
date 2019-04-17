45c7c711ba3d3550e679481250d5ee94b5daf8cd
DanielaNoe <daniela-noe@gmx.de>
2019-04-17 18:19:57 +0200

    Member-Variablen hinzugefügt

--- source/vec2.hpp
+++ source/vec2.hpp
@@ -3,7 +3,7 @@
 // Vec2 data type definition
 struct Vec2
 {
-/* TODO add member variables with
-default member initialisation */
+    Vec2 a ; // requires that 0.0 f == a . x and 0.0 f == a . y
+    Vec2 b {5.1 f , -9.3 f }; /* requires that 5.1 f == approx ( a . y ) and -9.3 f == approx ( b . y ) */
 };
 # endif // VEC2_HPP
